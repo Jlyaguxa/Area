65e7e71929009cb8b805d22caf8081e242c7c102
Jlyaguxa <JlyaguxaWK@gmail.com>
2026-06-22 21:04:19 +0500

    bug fix with Font styles and modification to suit the author's original idea with design

--- README.md
+++ README.md
@@ -19,11 +19,3 @@
 - [ ] Добавление динамического контента (слайдеры, формы) на JavaScript
 - [ ] Интеграция с сервером на Python (FastAPI) для загрузки данных
 
-Автор - [Jlyaguxa](https://github.com/Jlyaguxa)
-
-
-Thats a project to create a layout for Area
-
-Visit https://jlyaguxa.github.io/Area/ to view web page
-
-The layout that i used to create the site can be found at the link https://www.figma.com/community/file/1487309170684591074/modern-product-launch
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burgerBtn = document.getElementById('header-top--burger');
-  const nav = document.getElementById('headerNav');
-  const nav_list = document.querySelector('.header-top--nav-list')
+  const nav = document.getElementById('headerTopInner');
   const overlay = document.getElementById('headerOverlay');
-
+  const links = nav.querySelectorAll('a');
+  const elements = document.querySelectorAll('.component');
+  const box = Array.from(document.querySelector('.header-top--inner'))
+  const headerBtn = document.querySelector('.header-top--btn')
+  const navList = document.querySelector('.header-top--nav')
   // Функция открытия/закрытия
   function toggleMenu() {
     burgerBtn.classList.toggle('active');
@@ -26,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Закрытие при клике на любую ссылку внутри меню
-  const links = nav.querySelectorAll('a');
   links.forEach(link => {
     link.addEventListener('click', () => {
       if (nav.classList.contains('active')) {
@@ -34,7 +36,26 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  
+  box.addEventListener('click', (e) => {
+    e.preventDefault();
+    box.classList.toggle('active');
+    if(box.classList.contains('active')){
+      headerBtn.style.maxHeight = headerBtn.scrollHeight + 'px';
+      navList.style.maxHeight = headerBtn.scrollHeight + 'px';
+    } else{
+      headerBtn.style.maxHeight = 0;
+      navList.style.maxHeight = 0;
+    }
+  })
 
-  
-  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
+  elements.forEach(el => observer.observe(el));
 });
